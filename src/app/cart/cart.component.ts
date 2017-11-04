f9dde8c8cc52cd6116071740b8f098be3f3435b5
Lucy20209060 <550502661@qq.com>
2017-11-04 13:22:29 +0800

    函数类型

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -301,6 +301,16 @@ export class CartComponent implements OnInit {
 			// department.printMeeting();
 			// department.generateReports(); // error: method doesn't exist on declared abstract type
 
+			function buildName(firstName: string, lastName = "Smith") {
+			    return firstName + " " + lastName;
+			}
+
+			let result1 = buildName("Bob");                  // works correctly now, returns "Bob Smith"
+			let result2 = buildName("Bob", undefined);       // still works, also returns "Bob Smith"
+			// let result3 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+			let result4 = buildName("Bob", "Adams");         // ah, just right
+			console.log(result2)
+
 	}
 
 }
