31d17cc50ae9b2725b70312de96ae422a663b101
Lucy20209060 <550502661@qq.com>
2017-11-04 11:03:12 +0800

    抽象类

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -237,32 +237,69 @@ export class CartComponent implements OnInit {
 			// console.log(howard.getElevatorPitch());
 			// console.log(howard.name); // error
 
-			let passcode = "secret passcode";
+			// let passcode = "secret passcode";
 
-			class Employee {
-			    private _fullName: string;
+			// class Employee {
+			//     private _fullName: string;
 
-			    get fullName(): string {
-			    	console.log(111)
-			        return this._fullName;
-			    }
+			//     get fullName(): string {
+			//     	console.log(111)
+			//         return this._fullName;
+			//     }
 
-			    set fullName(newName: string) {
-			    	console.log(222)
-			        if (passcode && passcode == "secret passcode") {
-			            this._fullName = newName;
-			        }
-			        else {
-			            console.log("Error: Unauthorized update of employee!");
-			        }
-			    }
-			}
+			//     set fullName(newName: string) {
+			//     	console.log(222)
+			//         if (passcode && passcode == "secret passcode") {
+			//             this._fullName = newName;
+			//         }
+			//         else {
+			//             console.log("Error: Unauthorized update of employee!");
+			//         }
+			//     }
+			// }
 
-			let employee = new Employee();
+			// let employee = new Employee();
 			// employee.fullName = "Bob Smith";	// 赋值 调用set
 			// if (employee.fullName) {			// 获取 调用get
 			    // console.log(employee.fullName);
 			// }
+
+
+			abstract class Department {
+
+			    constructor(public name: string) {
+			    }
+
+			    printName(): void {
+			        console.log('Department name: ' + this.name);
+			    }
+
+			    abstract printMeeting(): void; // 必须在派生类中实现
+
+			    // abstract generateReports():void;
+			}
+
+			class AccountingDepartment extends Department {
+
+			    constructor() {
+			        super('Accounting and Auditing'); // constructors in derived classes must call super()
+			    }
+
+			    printMeeting(): void {
+			        console.log('The Accounting Department meets each Monday at 10am.');
+			    }
+
+			    generateReports(): void {
+			        console.log('Generating accounting reports...');
+			    }
+			}
+
+			// let department: Department; // ok to create a reference to an abstract type
+			// // department = new Department(); // error: cannot create an instance of an abstract class
+			// department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
+			// department.printName();
+			// department.printMeeting();
+			// department.generateReports(); // error: method doesn't exist on declared abstract type
 
 	}
 
